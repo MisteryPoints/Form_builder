c0a4d587be270203977889520799164e3a7421fb
MisteryPoints <misterypoint2@gmail.com>
2024-04-14 23:11:32 -0400

    Adding Layout, form, prisma and more interactions to componentes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { esES } from '@clerk/localizations'
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider> 
-      <html lang="en">
+    <ClerkProvider localization={esES}> 
+      <html lang="es">
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
